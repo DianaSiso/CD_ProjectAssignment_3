db601a7d6b0b6de95a340a5871e5f7ed38ecd65f
Andreia <andreia.portela@ua.pt>
2021-05-04 10:24:05 +0100

    v1

--- src/broker.py
+++ src/broker.py
@@ -1,35 +1,54 @@
 """Message Broker"""
 from typing import Dict, List, Any, Tuple
+import socket
+from serializer import serializable
 
 class Broker:
     """Implementation of a PubSub Message Broker."""
-
+    
     def __init__(self):
         """Initialize broker."""
         self.canceled = False
         self._host = "localhost"
         self._port = 5000
+        self.topics={}
+        self.subs={}
         LOGGER.info("Listen @ %s:%s", self._host, self._port)
 
 
 
     def list_topics(self) -> List[str]:
         """Returns a list of strings containing all topics."""
+        return self.topics.keys()
 
     def get_topic(self, topic):
         """Returns the currently stored value in topic."""
+        return self.topics[topic]
 
     def put_topic(self, topic, value):
         """Store in topic the value."""
+        self.topics[topic]=[value]
+        
 
     def list_subscriptions(self, topic: str) -> List[socket.socket]:
         """Provide list of subscribers to a given topic."""
+        res=[]
+        for elem in self.subs[topic]:
+            res.append(elem[0])
+        return res
 
     def subscribe(self, topic: str, address: socket.socket, _format: Serializer = None):
         """Subscribe to topic by client in address."""
-
+        if topic in self.subs:
+            self.subs[topic].append((address,_format))
+        else:
+            self.subs[topic]=[(address,_format)]
+            
     def unsubscribe(self, topic, address):
         """Unsubscribe to topic by client in address."""
+        for elem in self.subs[topic]:
+            if address in elem: 
+                self.subs[topic].remove(elem)
 
 
     def run(self):
--- src/middleware.py
+++ src/middleware.py
@@ -2,6 +2,8 @@
 from collections.abc import Callable
 from enum import Enum
 from queue import LifoQueue, Empty
+from .broker import Broker
+import socket
 
 
 class MiddlewareType(Enum):
@@ -10,19 +12,24 @@ class MiddlewareType(Enum):
     CONSUMER = 1
     PRODUCER = 2
 
-
 class Queue:
     """Representation of Queue interface for both Consumers and Producers."""
 
     def __init__(self, topic, _type=MiddlewareType.CONSUMER):
         """Create Queue."""
+        self.queue= Queue.LifoQueue()
+        self.topic=topic
+        
 
 
     def push(self, value):
         """Sends data to broker. """
+        self.broker.put_topic(self.topic,value)
 
 
     def pull(self) -> (str, tuple):
+        data= self.broker.get_topic(self.topic)
+        #de onde vem o tuple
         """Receives (topic, data) from broker.
 
         Should BLOCK the consumer!"""
@@ -30,14 +37,17 @@ class Queue:
 
     def list_topics(self, callback: Callable):
         """Lists all topics available in the broker."""
+        self.broker.list_topics()
 
 
     def cancel(self):
         """Cancel subscription."""
+        self.broker.unsubscribe(self.topic) #precisamos de um endereço e serializer
 
 
 class JSONQueue(Queue):
     """Queue implementation with JSON based serialization."""
+    self.broker.subscribe(self.topic)#precisamos de um endereço e serializer
 
 
 
